1133e195b2936057dadafbc2ddd94cb172630f2a
notwiwi <116315646+notwiwi@users.noreply.github.com>
2023-09-28 15:58:05 -0300

    Add files via upload

---
+++ age-calculator/index.js
@@ -0,0 +1,29 @@
+const btnEl = document.getElementById ("btn")
+const birthdayEl = document.getElementById ("birthday")
+const resultEl = document.getElementById("result")
+
+
+function calculateAge(){
+    const birthdayValue = birthdayEl.value;
+    if(birthdayValue==""){
+        alert("Please enter your birthdate")
+    }else{
+        const age = getAge(birthdayValue)
+        resultEl.innerText = `You are ${age} ${age > 1 ? "years" : "year"} old`;
+    }
+}
+
+function getAge(birthdayValue){
+    const currentDate = new Date()
+    const birthDate = new Date(birthdayValue)
+    let age = currentDate.getFullYear()-birthDate.getFullYear();
+    const month = currentDate.getMonth()-birthDate.getMonth();
+
+    if(month<0 || (month === 0 && currentDate.getDate() < birthDate.getDate())){
+        age--
+    }
+
+    return age
+}
+
+btnEl.addEventListener("click", calculateAge)
